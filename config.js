b121b3598bb95fade23b9ef9bf8585bd508052ce
vupputurivenkatesh05-eng <vupputurivenkatesh05@gmail.com>
2026-08-18 12:16:11 +0530

    Refresh temporary Planner API tunnel

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // HTTPS endpoint for the Erection Planner API.
 // This is a temporary Cloudflare quick tunnel. Replace it with the permanent
 // API hostname before a production rollout.
-window.ERECTION_PLANNER_API_BASE = "https://whats-groups-automatic-qualifying.trycloudflare.com/api";
+window.ERECTION_PLANNER_API_BASE = "https://contacting-scoop-met-prescription.trycloudflare.com/api";
